72bb9fcf981388b0855a208cdaaea0b727e343f1
siddheshvar-2802 <mrsiddheshvarpatil@gmail.com>
2026-08-07 09:10:00 -0700

    converting gradle to maven.

--- src/main/java/com/example/SubPulse/SubPulseApplication.java
+++ src/main/java/com/example/SubPulse/SubPulseApplication.java
@@ -1,13 +1,22 @@
 package com.example.SubPulse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SubPulseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SubPulseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SubPulseApplication.class, args);
+        System.out.println("""
+                ========================================================
+                🚀 SubPulse started successfully!
+                ✅ Application is up and running.
+                🌐 Ready to accept requests.
+                ========================================================
+                """);
+    }
 
 }
