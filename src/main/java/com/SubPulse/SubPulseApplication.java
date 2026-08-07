115732e5cb55cf3d8681404927f95e50c21f41aa
siddheshvar-2802 <mrsiddheshvarpatil@gmail.com>
2026-08-07 09:49:10 -0700

    refactored.

--- src/main/java/com/SubPulse/SubPulseApplication.java
+++ src/main/java/com/SubPulse/SubPulseApplication.java
@@ -1,22 +1,26 @@
-package com.SubPulse;
+package com.subpulse;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
+@EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class SubPulseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SubPulseApplication.class, args);
-        System.out.println("""
+        log.info("""
                 ========================================================
-                🚀 SubPulse started successfully!
-                ✅ Application is up and running.
-                🌐 Ready to accept requests.
+                 🚀  SubPulse started successfully!
+                 ✅  Application is up and running.
+                 🌐  Swagger UI : http://localhost:8080/swagger-ui.html
+                 📖  API Docs   : http://localhost:8080/api-docs
                 ========================================================
                 """);
     }
-
 }
