4d905ae87ba3e2c290a04d7e6e1beedca1690560
siddheshvar-2802 <mrsiddheshvarpatil@gmail.com>
2026-08-07 08:12:24 -0700

    Initial commit

---
+++ src/main/java/com/example/SubPulse/SubPulseApplication.java
@@ -0,0 +1,13 @@
+package com.example.SubPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SubPulse/SubPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SubPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
